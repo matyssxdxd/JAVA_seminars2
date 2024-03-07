ca766f2de875a107b1d1cd1ea03f6ab111db6a86
matyss <matissp1302@gmail.com>
2024-03-07 10:14:19 +0200

    update good

--- src/main/VeA_IS.java
+++ src/main/VeA_IS.java
@@ -20,10 +20,10 @@ public class VeA_IS {
     private static final Grade grad2 = new Grade(10, stud1, course2);
 
 
-    private static ArrayList<Professor> profList = new ArrayList<>(Arrays.asList(prof, prof1));
-    private static ArrayList<Student> studList = new ArrayList<>(Arrays.asList(stud, stud1));
-    private static ArrayList<Course> courseList = new ArrayList<>(Arrays.asList(course, course1, course2));
-    private static ArrayList<Grade> gradeList = new ArrayList<>(Arrays.asList(grad, grad1, grad2));
+    private static final ArrayList<Professor> profList = new ArrayList<>(Arrays.asList(prof, prof1));
+    private static final ArrayList<Student> studList = new ArrayList<>(Arrays.asList(stud, stud1));
+    private static final ArrayList<Course> courseList = new ArrayList<>(Arrays.asList(course, course1, course2));
+    private static final ArrayList<Grade> gradeList = new ArrayList<>(Arrays.asList(grad, grad1, grad2));
 
     public static void main(String[] args) {
         try {
@@ -36,6 +36,20 @@ public class VeA_IS {
         try {
             float weightedAverageGrade = weightedAverageGrade(stud1);
             System.out.println(weightedAverageGrade);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            float averageGradeOfCourse = calculateAverageGradeOfCourse(course1);
+            System.out.println(averageGradeOfCourse);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            int professorCourses = calcualteProfessorCourses(prof1);
+            System.out.println(professorCourses);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -75,5 +89,80 @@ public class VeA_IS {
         if (howMany == 0) throw new Exception("nava kursi");
 
         return sum / howMany;
+    }
+
+    public static float calculateAverageGradeOfCourse(Course course) throws Exception {
+        if (course == null) throw new Exception("NAE NAE");
+        float sum = 0;
+        int howMany = 0;
+
+        for (Grade tempGr : gradeList) {
+            if (tempGr.getCourse().equals(course)) {
+                sum += tempGr.getValue();
+                howMany++;
+            }
+        }
+
+        if (howMany == 0) throw new Exception("WEE WOO");
+
+        return sum / howMany;
+    }
+
+    public static int calcualteProfessorCourses(Professor professor) throws Exception {
+        if (professor == null) throw new Exception("WAE WAE");
+        int howMany = 0;
+
+        for (Course course : courseList) {
+            if (course.getProfessor().equals(professor)) {
+                howMany += 1;
+            }
+        }
+        if (howMany == 0) throw new Exception("WEE WOO");
+
+        return howMany;
+    }
+
+    public static void createStudent(String name, String surname) throws Exception {
+        if (name == null || surname == null) throw new Exception("NAA MAN");
+
+        for (Student student : studList) {
+            if (student.getName().equals(name) && student.getSurname().equals(surname)) {
+                throw new Exception("BABA");
+            }
+        }
+
+        Student st = new Student(name, surname);
+        studList.add(st);
+    }
+
+    public static Student retrieveStudentBySurname(String surname) throws Exception {
+        if (surname == null) throw new Exception("AHAHA");
+
+        for (Student student : studList) {
+            if (student.getSurname().equals(surname)) {
+                return student;
+            }
+        }
+
+        throw new Exception("BLABLA");
+    }
+
+    public static void updateStudentByNameAndSurname(String name, String surname, String newSurname) throws Exception {
+        if (name == null || surname == null || newSurname == null) throw new Exception("BLABLA");
+
+        for (Student student : studList) {
+            if (student.getName().equals(name) && student.getSurname().equals(surname)) {
+                if (!surname.equals(newSurname)) {
+                    student.setSurname(newSurname);
+                    return;
+                }
+            }
+        }
+
+        throw new Exception("BEE");
+    }
+
+    public static void sortStudentsByAverageGrade() throws Exception {
+
     }
 }
